a8970708fa7174d6778d884cb032f3e409cfaa57
Koohyun <bassdesire@gmail.com>
2021-04-25 16:43:21 -0400

    Add Holder weight function
    Add a function named w_get_Hol() which calculates the optimal weights and bandwidths for inference for regression function values in Holder spaces

---
+++ R/weights.R
@@ -0,0 +1,55 @@
+#' Optimal weights for Hölder space
+#'
+#' Computes optimal weights and bandwidths for inference for nonparametric regression function values
+#' under Hölder space, with an aid of \code{RDHonest} package.
+#'
+#' @param y vector of dependent variables
+#' @param x vector of regressors
+#' @param eval evaluation points
+#' @param M bound on the second derivative
+#' @param level confidence level
+#' @param kern specifies kernel function used in the local regression; default = \code{"triangular"}.
+#' See \code{\link[RDHonest]{NPROptBW.fit}} for a list of kernels available.
+#' @param se.initial method for estimating initial variance for computing optimal bandwidt; default = \code{"EHW"}.
+#' See \code{\link[RDHonest]{NPROptBW.fit}} for a list of method available.
+#' @param se.method methods for estimating standard error of estimate; default = \code{"nn"}.
+#' See \code{\link[RDHonest]{NPRreg.fit}} for a list of method available.
+#' @param J number of nearest neighbors, if "nn" is specified in se.method.
+#'
+#' @return list with components
+#'
+#' \describe{
+#'     \item{w.mat}{matrix of optimal weigths with a dimension \code{length(y)} by
+#'     \code{length(eval)} corresponding to the evaluation points}
+#'     \item{bw.vec}{vector of optimal bandwidths corresponding to the evaluation points}
+#'
+#' }
+#' @export
+#'
+#' @examples x <- stats::runif(500, min = -1, max = 1)
+#' y <- x + rnorm(500, 0, 1/4)
+#' eval <- seq(from = -0.9, to = 0.9, length.out = 5)
+#' w_get_Hol(y, x, eval, 1, 0.95)
+w_get_Hol <- function(y, x, eval, M, level, kern = "triangular", se.initial = "EHW", se.method = "nn", J = 3){
+
+  n <- length(y)
+  m <- length(eval)
+  bw.vec <- numeric(m)
+  w.mat <- matrix(0, nrow = n, ncol = m)
+
+  for(i in 1:m){
+
+    d <- RDHonest::LPPData(as.data.frame(cbind(y, x)), point = eval[i])
+
+    bw.vec.i <- RDHonest::NPROptBW.fit(d, M = M, kern = kern, opt.criterion = "OCI", alpha = 1 - level,
+                                      beta = 0.5, se.initial = se.initial)
+    bw.vec[i] <- bw.vec.i$h[1]
+
+    w.mat.i <- RDHonest::NPRreg.fit(d, bw.vec[i], kern = kern, se.method = se.method, J = J)
+    w.mat[, i] <- w.mat.i$w
+  }
+
+  res <- list(w.mat = w.mat, bw.vec = bw.vec)
+
+  return(res)
+}
