279f0a516476ffce8884a24871c9261b46f9f172
Koohyun <bassdesire@gmail.com>
2021-04-28 14:00:05 -0400

    change testthat for quantile allowing positive covariances

--- tests/testthat/test-quantile.R
+++ tests/testthat/test-quantile.R
@@ -67,10 +67,12 @@ test_that("positive estimated variance (k = 2)",{
   n <- 500
   sd <- 0.1
   x.1 <- x.0 <- seq(from = -1, to = 1, length.out = n)
-  y.11 <- x.1^2 + stats::rnorm(n, 0, sd)
-  y.12 <- x.1 + stats::rnorm(n, 0, sd)
-  y.01 <- x.0^2 + stats::rnorm(n, 0, sd)
-  y.02 <- x.0 + stats::rnorm(n, 0, sd)
+  eps.1 <- stats::rnorm(n, 0, sd)
+  y.11 <- x.1^2 + eps.1 + stats::rnorm(n, 0, sd)
+  y.12 <- x.1 + eps.1 + stats::rnorm(n, 0, sd)
+  eps.0 <- stats::rnorm(n, 0, sd)
+  y.01 <- x.0^2 + eps.0 + stats::rnorm(n, 0, sd)
+  y.02 <- x.0 + eps.0 + stats::rnorm(n, 0, sd)
   y.1 <- cbind(y.11, y.12)
   y.0 <- cbind(y.01, y.02)
   w.1 <- w.0 <- cbind(rep(1/n, n), rep(1/n, n))
@@ -80,7 +82,7 @@ test_that("positive estimated variance (k = 2)",{
 
   res <- stud_err_sim(y.1, y.0, x.1, x.0, w.1, w.0, c(1, 1), 1, "triangle", TRUE, z.1, z.0)$dnmnt
 
-  omega.1 <- omega.0 <- array(rep(c(sd^2,0,0,sd^2), each = n), dim = c(n, 2, 2))
+  omega.1 <- omega.0 <- array(rep(c(2 * sd^2, sd^2, sd^2 , 2 * sd^2), each = n), dim = c(n, 2, 2))
   res.true <- avar(w.1, w.0, omega.1, omega.0, c(1, 1))
   c(res, sqrt(res.true))
 
