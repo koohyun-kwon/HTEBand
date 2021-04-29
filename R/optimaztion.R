25e01be58b1a225c7dab6eb9d642a37859982a27
Koohyun <bassdesire@gmail.com>
2021-04-29 16:03:40 -0400

    Add opt_w(), a function which calculates the optimal weigths

---
+++ R/optimaztion.R
@@ -0,0 +1,97 @@
+#' Optimal weights
+#'
+#' Solve for optimal weigths by the quantile matching method
+#'
+#' @param method character string used to define the parameter of interest and the function space.
+#' Currently, only \code{"reg.Hol"} is supported.
+#' @param C.vec smoothness parameter for the function space; possibly a vector.
+#' @param y dependent variable; possibly a matrix with \code{nrow(y)} equals the number of observations.
+#' @param x a vector of independent variable
+#' @param d a vector of treatment indicator; it can be arbitrarily specified when not used.
+#' @param eval a vector of indices
+#' @param root.robust if \code{TRUE}, the fuction conducts diagnostic test whether optimizaiton worked well;
+#' default is \code{root.robust = FALSE}.
+#' @param ng the number of grids of quantile values over which the diagnostic test would be peformed
+#' if \code{root.robust = TRUE}; default is \code{ng = 10}.
+#' @inheritParams sup_quant_sim
+#'
+#' @return a list of the following components
+#' \describe{
+#' \item{w.1}{a 3-dim array of weigths corresponding to the treated observations;
+#' the second and third dimension correponds to \code{ncol(y)} and \code{length(eval)}}
+#' \item{w.0}{a 3-dim array of weigths corresponding to the control observations;
+#' the second and third dimension correponds to \code{ncol(y)} and \code{length(eval)}.
+#' If there is no control observations, returns array of 0.}
+#' \item{c.root}{the quantile value corresponding to the optimal weigths.}
+#' \item{increasing}{boolean value testing whether the objective function is increasing or not;
+#' provided only if \code{root.robust = TRUE}}
+#' \item{opt.grid}{a \code{data.frame} object containing evaluations of the objective function
+#' over grid values of quantiles; provided only if \code{root.robust = TRUE}}
+#' }
+#' @export
+opt_w <- function(method, C.vec, y, x, d, eval, T.grad.mat, level,
+                  deg, kern, loo, M, seed = NULL, useloop = TRUE,
+                  root.robust = FALSE, ng = 10){
+
+  n.T <- length(eval)
+
+  if(method == "reg.Hol"){
+
+    kern.reg <- "triangular"
+    se.initial <- "EHW"
+    se.method <- "nn"
+    J <- 3
+    C <- C.vec[1]
+  }
+
+  eq <- function(c){
+
+    level.int <- stats::pnorm(2 * c)
+    if(method == "reg.Hol"){
+
+      w.1 <- w_get_Hol(y, x, eval, C, level.int, kern.reg, se.initial, se.method, J)$w.mat
+      w.1 <- array(w.1, dim = c(length(y), 1, n.T))
+      w.0 <- array(0, dim = c(1, 1, n.T))
+
+      q.sim <- sup_quant_sim(y, 0, x, 0, w.1, w.0, rep(1, n.T),
+                             level, deg, kern, loo, M, seed, useloop)
+    }
+
+    return(c - q.sim)
+  }
+
+  c.min <- stats::qnorm(level)
+  c.max <- stats::qnorm(1 - (1 - level)/(2 * n.T)) + 1  # Takes into account some numerical errors
+
+  root.res <- stats::uniroot(eq, interval = c(c.min, c.max))
+  c.root <- root.res$root
+
+  if(method == "reg.Hol"){
+
+    w.1 <- w_get_Hol(y, x, eval, C, stats::pnorm(2 * c.root),
+                     kern.reg, se.initial, se.method, J)$w.mat
+    w.1 <- array(w.1, dim = c(length(y), 1, n.T))
+    w.0 <- array(0, dim = c(1, 1, n.T))
+  }
+
+  if(root.robust){
+
+    c.grid <- seq(from = c.min, to = c.max, length.out = ng)
+    obj.val <- numeric(ng)
+
+    for(i in 1:ng) obj.val[i] = eq(c.grid[i])
+
+    is.inc <- !is.unsorted(obj.val)
+    opt.grid <- data.frame(c.grid = c.grid, obj.val = obj.val)
+
+    res <- list(w.1 = w.1, w.0 = w.0, c.root = c.root, increasing = is.inc, opt.grid = opt.grid)
+
+  }else{
+
+    res <- list(w.1 = w.1, w.0 = w.0, c.root = c.root)
+  }
+
+
+  return(res)
+
+}
---
+++ tests/testthat/test-optimaztion.R
@@ -0,0 +1,56 @@
+test_that("valid optimal quantile value", {
+
+  n <- 500
+  M <- 500
+  x <- stats::runif(n, min = -1, max = 1)
+  n.T <- 10
+  eval <- seq(from = -0.9, to = 0.9, length.out = n.T)
+  T.grad.mat <- rep(1, n.T)
+  level <- 0.95
+  y <- x + rnorm(n, 0, 1/4)
+
+  res <- NA
+
+  system.time({
+    opt.res <- opt_w("reg.Hol", 1, y, x, 0, eval, T.grad.mat, level,
+                      1, "triangle", FALSE, M, seed = NULL, useloop = TRUE,
+                      root.robust = TRUE)
+    res <- opt.res$c.root
+    res.inc <- opt.res$increasing
+  })
+
+  expect_equal(!is.na(res), TRUE)
+  expect_equal(res.inc, TRUE)
+
+  # Optional test
+
+  # kern.reg <- "triangular"
+  # se.initial <- "EHW"
+  # se.method <- "nn"
+  # J <- 3
+  # C <- 1
+  #
+  # eq <- function(c){
+  #
+  #   level.int <- stats::pnorm(2 * c)
+  #   w.1 <- w_get_Hol(y, x, eval, C, level.int, kern.reg, se.initial, se.method, J)$w.mat
+  #   w.1 <- array(w.1, dim = c(length(y), 1, n.T))
+  #   w.0 <- array(0, dim = c(1, 1, n.T))
+  #
+  #   q.sim <- sup_quant_sim(y, 0, x, 0, w.1, w.0, rep(1, n.T), level,
+  #                          1, "triangle", FALSE, M, seed = NULL, useloop = TRUE)
+  #   return(c - q.sim)
+  # }
+  #
+  # c.min <- stats::qnorm(level)
+  # c.max <- stats::qnorm(1 - (1 - level)/(2 * n.T)) + 1
+  #
+  # res <- numeric(10)
+  # grid <- seq(from = c.min, to = c.max, length.out = 10)
+  # for(i in 1:10){
+  #   print(i)
+  #   res[i] <- eq(grid[i])
+  # }
+  #
+  # plot(grid, res, type = "l")
+})
