1460ba0d779bfee0c1a06ce5150fe91c4b9a914d
Koohyun <bassdesire@gmail.com>
2021-05-04 17:37:17 -0400

    Add sd_Lip() and minor change

--- R/opt_bw.R
+++ R/opt_bw.R
@@ -1,6 +1,6 @@
 #' Lipschitz class worst-case bias
 #'
-#' Calculates the worst-case bias for regression function values under Lipschitz class
+#' Calculates the worst-case bias for regression function value estimator under Lipschitz class
 #'
 #' @inheritParams K_fun
 #' @param M bound on the first derivative
@@ -14,6 +14,25 @@ bias_Lip <- function(x, t, M, kern, h){
   dnmnt <- sum(K_fun(x, t, h, kern))
 
   return(nmrt / dnmnt)
+}
+
+#' Lipschitz class standard deviation
+#'
+#' Calculates the standard deviation for regression function value estimator under Lipschitz class
+#'
+#' @inheritParams K_fun
+#' @inheritParams eps_hat
+#'
+#' @return a scalar standard deviation value
+#' @export
+sd_Lip <- function(y, x, t, kern, h, deg, loo){
+
+  sd.hat <- eps_hat(y, x, deg, kern, loo)
+
+  nmrt <- sum(K_fun(x, t, h, kern)^2 * sd.hat^2)
+  dnmnt <- sum(K_fun(x, t, h, kern))^2
+
+  return(sqrt(nmrt / dnmnt))
 }
 
 # bw_Lip <- function(y, x, t, TE, d, M, kern, alpha, beta, se.initial, bw.eq){
--- tests/testthat/test-opt_bw.R
+++ tests/testthat/test-opt_bw.R
@@ -2,3 +2,11 @@ test_that("positive worst case bias", {
   x <- seq(-1, 1, length.out = 100)
   expect_equal(bias_Lip(x, 0, 1, "triangle", 0.1) > 0, TRUE)
 })
+
+
+test_that("positive standard deviation", {
+  x <- seq(-1, 1, length.out = 100)
+  eps <- rnorm(100, 0, 1/2 + x^2)
+  y <- x + eps
+  expect_equal(sd_Lip(y, x, 0, "triangle", 0.1, 0, FALSE) > 0, TRUE)
+})
