080a66c14068cc325675c72a44878ed7f03f02bd
Koohyun <bassdesire@gmail.com>
2021-07-18 16:19:15 -0400

    make ci_reg_Hol to deal with CATE

--- R/ci_const.R
+++ R/ci_const.R
@@ -11,6 +11,7 @@
 #' @param cv supplied value of critical value to be used in constructing confidence interval;
 #' default is \code{cv = NULL}.
 #' @inheritParams w_get_Hol
+#' @inheritParams bw_opt
 #'
 #' @return a vector of lower and upper ends of the confidence interval  and a pair of bandwidths used for
 #' the treatment and control groups.
@@ -23,26 +24,70 @@
 #' y <- x + rnorm(500, 0, 1/4)
 #' ci_reg_Hol(y, x, 1/2, 1, 0.99)
 ci_reg_Hol <- function(y, x, point, C, level, kern = "triangular", se.initial = "EHW",
-                       se.method = "nn", J = 3, cv = NULL){
+                       se.method = "nn", J = 3, cv = NULL, TE = FALSE, d = NULL, bw.eq = TRUE){
 
-  d <- RDHonest::LPPData(as.data.frame(cbind(y, x)), point)
+  if(TE){
 
-  ci.res <- RDHonest::NPRHonest.fit(d = d, M = C, kern = kern, opt.criterion = "OCI",
-                                    alpha = 1 - level, beta = 0.5, se.method = se.method, J = J,
-                                    se.initial = se.initial)
+    try(if(is.null(d)) stop("Supply treatment indicator in d"))
 
-  maxbias <- ci.res$maxbias
-  sd <- unname(ci.res$sd)
-  if(is.null(cv)){
-    c <- stats::qnorm(level)/2  # We are constructing two-sided, not one-sided, CI.
+    if(kern == "triangular"){
+      kern.rdh <- kern
+      kern <- "tri"
+    }
+
+    opt.res <- bw_opt(y, x, point, TE, d, C, kern, 1 - level, bw.eq, p = 2)
+    h.opt <- opt.res$h.opt
+
+    y.1 <- y[d == 1]
+    y.0 <- y[d == 0]
+    x.1 <- x[d == 1]
+    x.0 <- x[d == 0]
+    d.1 <- RDHonest::LPPData(as.data.frame(cbind(y.1, x.1)), point)
+    d.0 <- RDHonest::LPPData(as.data.frame(cbind(y.0, x.0)), point)
+
+    ci.res.1 <- RDHonest::NPRHonest.fit(d = d.1, M = C, kern = kern.rdh, opt.criterion = "OCI",
+                                      alpha = 1 - level, beta = 0.5, se.method = se.method, J = J,
+                                      se.initial = se.initial)
+    ci.res.0 <- RDHonest::NPRHonest.fit(d = d.0, M = C, kern = kern.rdh, opt.criterion = "OCI",
+                                        alpha = 1 - level, beta = 0.5, se.method = se.method, J = J,
+                                        se.initial = se.initial)
+
+    est <- ci.res.1$estimate - ci.res.0$estimate
+    maxbias <- ci.res.1$maxbias + ci.res.0$maxbias
+    sd <- sqrt(unname(ci.res.1$sd)^2 + unname(ci.res.0$sd)^2)
+
+    if(is.null(cv)){
+      c <- stats::qnorm(level)/2  # We are constructing two-sided, not one-sided, CI.
+    }else{
+      c <- cv
+    }
+
+    hl <- maxbias + sd * c
+    ci.lower <- est - hl
+    ci.upper <- est + hl
+
+    return(c(ci.lower, ci.upper, h.opt))
   }else{
-    c <- cv
-  }
-  hl <- maxbias + sd * c
-  ci.lower <- ci.res$estimate - hl
-  ci.upper <- ci.res$estimate + hl
 
-  return(c(ci.lower, ci.upper, ci.res$hp, ci.res$hp))
+    d <- RDHonest::LPPData(as.data.frame(cbind(y, x)), point)
+
+    ci.res <- RDHonest::NPRHonest.fit(d = d, M = C, kern = kern, opt.criterion = "OCI",
+                                      alpha = 1 - level, beta = 0.5, se.method = se.method, J = J,
+                                      se.initial = se.initial)
+
+    maxbias <- ci.res$maxbias
+    sd <- unname(ci.res$sd)
+    if(is.null(cv)){
+      c <- stats::qnorm(level)/2  # We are constructing two-sided, not one-sided, CI.
+    }else{
+      c <- cv
+    }
+    hl <- maxbias + sd * c
+    ci.lower <- ci.res$estimate - hl
+    ci.upper <- ci.res$estimate + hl
+
+    return(c(ci.lower, ci.upper, ci.res$hp, ci.res$hp))
+  }
 }
 
 #' Confidence interval for regression function value for Lipschitz space
--- tests/testthat/test-ci_const.R
+++ tests/testthat/test-ci_const.R
@@ -23,3 +23,24 @@ test_that("valid confidence interval - Lipschitz regression", {
   expect_equal(!is.na(res), rep(TRUE, 4))
   expect_equal(!is.na(res.eq), rep(TRUE, 4))
 })
+
+
+test_that("valid confidence interval - Hölder regression", {
+
+  n <- 250
+  x.1 <- x.0 <- seq(-1, 1, length.out = n)
+  sd.true <- 1/2 + x.1^2
+  eps <- stats::rnorm(n, 0, sd.true)
+  y.1 <- x.1 + eps
+  y.0 <- x.1^2 + eps/2
+
+  y <- c(y.1, y.0)
+  x <- c(x.1, x.0)
+  d <- rep(c(1, 0), each = n)
+
+  res.eq <- ci_reg_Hol(y, x, 0, 2, 0.99, TE = TRUE, d = d,
+                       bw.eq = TRUE)
+  res.eq
+
+  expect_equal(!is.na(res.eq), rep(TRUE, 4))
+})
